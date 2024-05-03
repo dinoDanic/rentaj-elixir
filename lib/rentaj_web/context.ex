b7f8a44e9e0499af4e46ab8f43f43dc31ed728d7
Dino Danic <dino.danic@gmail.com>
2024-05-03 18:32:18 +0200

    fix get user again

--- lib/rentaj_web/context.ex
+++ lib/rentaj_web/context.ex
@@ -16,10 +16,6 @@ defmodule RentajWeb.Context do
   Return the current user context based on the authorization header
   """
   def build_context(conn) do
-    IO.puts(~c"tu samjmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm ")
-    tk = get_req_header(conn, "authorization")
-    IO.inspect(tk)
-
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
          {:ok, claims} <- Rentaj.Guardian.decode_and_verify(token),
          {:ok, current_user} <- resource_from_claims(claims) do
@@ -30,11 +26,11 @@ defmodule RentajWeb.Context do
   end
 
   defp resource_from_claims(%{"sub" => id}) do
-    user = Accounts.get_user(id)
+    user = Accounts.get_user!(id)
 
     case user do
-      {:ok, user} -> {:ok, user}
-      _ -> {:error, "User not found"}
+      nil -> {:error, "User not found"}
+      _ -> {:ok, user}
     end
   end
 end
--- lib/rentaj_web/middleare.ex
+++ lib/rentaj_web/middleare.ex
@@ -2,6 +2,10 @@ defmodule RentajWeb.Middleare do
   @behaviour Absinthe.Middleware
 
   def call(resolution, _config) do
+    IO.puts("2. MIDLEWARE")
+    IO.inspect(resolution.context)
+    IO.puts("2. MIDLEWARE END")
+
     case resolution.context do
       %{current_user: _} ->
         resolution
