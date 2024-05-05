65a316d100e1da12262da374aa6b9c28ba46f7e5
Dino Danic <dino.danic@gmail.com>
2024-05-05 16:43:18 +0200

    remove prod exs setup

--- config/prod.exs
+++ config/prod.exs
@@ -1,25 +1,5 @@
 import Config
 
-# config :rentaj, Rentaj.Repo,
-#   username: System.get_env("PGUSER"),
-#   password: System.get_env("PGPASSWORD"),
-#   hostname: System.get_env("PGHOST"),
-#   database: System.get_env("PGDATABASE"),
-#   port: System.get_env("PGPORT"),
-#   stacktrace: false,
-#   show_sensitive_data_on_connection_error: false,
-#   pool_size: 10
-#
-# config :rentaj, Rentaj.Endpoint,
-#   # Binding to loopback ipv4 address prevents access from other machines.
-#   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-#   http: [ip: {0, 0, 0, 0}, port: 4000],
-#   check_origin: false,
-#   code_reloader: true,
-#   debug_errors: true,
-#   secret_key_base: "uvwuQObU9x/xGbok+rb2N2tqLpeCGqckq0SYH1vcFJNiM3gymmSYYX2GDCqCjPSG",
-#   watchers: []
-
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Rentaj.Finch
 
