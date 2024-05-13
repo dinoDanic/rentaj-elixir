5c75863a2890970a50930e3f3d48c3f487e6a6cd
Dino Danic <dino.danic@gmail.com>
2024-05-13 20:05:54 +0200

    desction, price ddelivery active more on itmes

--- lib/rentaj/items/item.ex
+++ lib/rentaj/items/item.ex
@@ -7,6 +7,9 @@ defmodule Rentaj.Items.Item do
     field :price_per_day, :decimal
     field :delivery, :boolean
     field :pick_up, :boolean
+    field :capara, :decimal
+    field :description, :string
+    field :active, :boolean
 
     belongs_to :category, Rentaj.Categories.Category
     belongs_to :user, Rentaj.Accounts.User
@@ -17,7 +20,17 @@ defmodule Rentaj.Items.Item do
   @doc false
   def changeset(item, attrs) do
     item
-    |> cast(attrs, [:name, :category_id, :user_id])
-    |> validate_required([:name, :category_id, :user_id])
+    |> cast(attrs, [
+      :name,
+      :category_id,
+      :user_id,
+      :price_per_day,
+      :delivery,
+      :pick_up,
+      :capara,
+      :description,
+      :active
+    ])
+    |> validate_required([:name, :category_id, :user_id, :price_per_day, :active])
   end
 end
--- priv/repo/migrations/20240505153938_create_items.exs
+++ priv/repo/migrations/20240505153938_create_items.exs
@@ -4,8 +4,14 @@ defmodule Rentaj.Repo.Migrations.CreateItems do
   def change do
     create table(:items) do
       add :name, :string
+      add :description, :string 
       add :category_id, references(:categories, on_delete: :nothing)
       add :user_id, references(:users, on_delete: :nothing)
+      add :price_per_day, :decimal, precision: 10, scale: 2, null: false
+      add :delivery, :bool, default: false, null: false
+      add :active, :bool, default: true, null: false
+      add :pick_up, :bool, default: false, null: false
+      add :capara, :decimal, precision: 10, scale: 2, null: true
 
       timestamps(type: :utc_datetime)
     end
