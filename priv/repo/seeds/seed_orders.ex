7a805a04b952950af004fdfc811701e192811093
Dino Danic <dino.danic@gmail.com>
2024-05-18 06:50:01 +0200

    seeds

--- priv/repo/seeds/seed_orders.ex
+++ priv/repo/seeds/seed_orders.ex
@@ -1,4 +1,16 @@
 alias Rentaj.Orders.Order
 alias Rentaj.Repo
 
-%Order{item_id: 1, renter_id: 1, customer_id: 2, start_date: "2024-05-13 22:10:42", end_date: "2024-05-15 22:10:44", delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+{:ok, start_date1, 0} = DateTime.from_iso8601("2024-05-13T22:10:42Z")
+{:ok, end_date1, 0} = DateTime.from_iso8601("2024-05-15T22:10:44Z")
+
+{:ok, start_date2, 0} = DateTime.from_iso8601("2024-05-17T22:10:42Z")
+{:ok, end_date2, 0} = DateTime.from_iso8601("2024-05-23T22:10:44Z")
+
+
+%Order{item_id: 1, renter_id: 1, customer_id: 2, start_date: start_date1, end_date: end_date1, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+%Order{item_id: 2, renter_id: 2, customer_id: 2, start_date: start_date1, end_date: end_date1, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+%Order{item_id: 3, renter_id: 3, customer_id: 3, start_date: start_date2, end_date: end_date2, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+%Order{item_id: 4, renter_id: 1, customer_id: 2, start_date: start_date2, end_date: end_date2, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+%Order{item_id: 5, renter_id: 1, customer_id: 5, start_date: start_date2, end_date: end_date2, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
+%Order{item_id: 6, renter_id: 1, customer_id: 4, start_date: start_date2, end_date: end_date2, delivery: true, pick_up: false, status: :draft} |> Repo.insert!()
